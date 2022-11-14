8a293e8c0ac4122b91199677d4c6488e0e6841a8
Edward A. Roualdes <eroualdes@csuchico.edu>
2022-11-14 11:52:55 -0800

    fix comparisons, all seasonally unadjusted; pick CPI less energy series with more data

--- script.R
+++ script.R
@@ -11,11 +11,11 @@ library(ggplot2)
 ## Glossary
 ## https://www.bls.gov/cex/csxgloss.htm
 
-## CUSR0000SAH => Housing
-## CUSR0000SAM2 => Medical care
+## CUUR0000SAH => Housing
+## CUUR0000SAM => Medical care
 ## CUUR0000SERG02 => Recreational books
 ## CUUR0000SSEA011 => College textbooks
-## CUURD000SA0LE => CPI (everything but energy)
+## CUUR0000SA0LE => CPI (everything but energy)
 
 wdths <- fwf_widths(c(17, 4, 3, 12, 10)+1,
                     col_names = c("series_id", "year", "period", "value", "footnotes"))
@@ -23,15 +23,16 @@ df <- read_fwf("https://download.bls.gov/pub/time.series/cu/cu.data.0.Current",
                col_positions = wdths,
                skip = 1)
 
-
 df2 <- df %>%
-    filter(str_detect(series_id, "^CUUR0000SSEA011$|^CUUR0000SERG02$|^CUSR0000SAM$|^CUSR0000SAH$|^CUURD000SA0LE$")) %>%
+    filter(series_id %in% c("CUUR0000SAH", "CUUR0000SAM", "CUUR0000SERG02", "CUUR0000SSEA011", "CUUR0000SA0LE")) %>%
     mutate(month = month(period(month = str_match(period, "[\\d]{2}"), units = "month"))) %>%
     filter(month < 13) %>%
     mutate(Date = ymd(paste(year, month, "01", sep="-")),
            Series = factor(series_id,
-                           labels = c("Housing", "Medical care",
-                                      "Recreational books", "College textbooks",
+                           labels = c("Housing",
+                                      "Medical care",
+                                      "Recreational books",
+                                      "College textbooks",
                                       "CPI (no energy)")))
 
 
